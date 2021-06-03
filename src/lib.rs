9812a9015016bcbe9c94dd56c9576bbe5fae2808
Kane Rogers <kane.m.rogers@gmail.com>
2021-06-03 17:30:00 +1000

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
-use ash::vk;
+use ash::{version::InstanceV1_0, vk};
 use cgmath::Vector3;
 use hotham_error::HothamError;
 use openxr as xr;
 use renderer::Renderer;
 use std::{path::Path, thread::sleep, time::Duration};
+use xr::{vulkan::SessionCreateInfo, Session, Vulkan};
 
 use crate::vulkan_context::VulkanContext;
 
+mod frame;
 mod hotham_error;
 mod renderer;
 mod swapchain;
@@ -18,12 +20,12 @@ pub const COLOR_FORMAT: vk::Format = vk::Format::R8G8B8A8_UNORM;
 pub const VIEW_COUNT: u32 = 2;
 pub const VIEW_TYPE: xr::ViewConfigurationType = xr::ViewConfigurationType::PRIMARY_STEREO;
 
-#[derive(Clone)]
 pub struct App<P: Program> {
     program: P,
     should_quit: bool,
     renderer: Renderer,
-    instance: openxr::Instance,
+    xr_instance: openxr::Instance,
+    xr_session: Session<Vulkan>,
 }
 
 #[derive(Clone, Debug)]
@@ -45,36 +47,21 @@ where
     pub fn new(program: P) -> Result<Self> {
         let params = program.init();
         println!("Initialised program with {:?}", params);
-        let xr_entry = xr::Entry::linked();
-        let xr_app_info = openxr::ApplicationInfo {
-            application_name: "Hotham Cubeworld",
-            application_version: 1,
-            engine_name: "Hotham",
-            engine_version: 1,
-        };
-        let mut required_extensions = xr::ExtensionSet::default();
-        required_extensions.khr_vulkan_enable2 = true;
-        let instance = xr_entry.create_instance(&xr_app_info, &required_extensions, &[])?;
-        let system = instance.system(xr::FormFactor::HEAD_MOUNTED_DISPLAY)?;
+        let xr_instance = create_xr_instance()?;
+        let system = xr_instance.system(xr::FormFactor::HEAD_MOUNTED_DISPLAY)?;
         let _environment_blend_mode =
-            instance.enumerate_environment_blend_modes(system, VIEW_TYPE)?[0];
+            xr_instance.enumerate_environment_blend_modes(system, VIEW_TYPE)?[0];
 
-        let vk_target_version_xr = xr::Version::new(1, 1, 0);
-
-        let requirements = instance.graphics_requirements::<xr::Vulkan>(system)?;
-        if vk_target_version_xr < requirements.min_api_version_supported
-            || vk_target_version_xr.major() > requirements.max_api_version_supported.major()
-        {
-            return Err(HothamError::UnsupportedVersionError);
-        }
-
-        let vulkan_context = VulkanContext::create_from_xr_instance(&instance, system)?;
+        let vulkan_context = VulkanContext::create_from_xr_instance(&xr_instance, system)?;
+        let xr_session = create_xr_session(&xr_instance, system, &vulkan_context)?;
+        let renderer = Renderer::new(vulkan_context, &xr_session)?;
 
         Ok(Self {
             program,
-            renderer: Renderer::new(vulkan_context),
+            renderer,
             should_quit: false,
-            instance,
+            xr_instance,
+            xr_session,
         })
     }
 
@@ -88,6 +75,41 @@ where
             sleep(Duration::from_secs(1))
         }
     }
+}
+
+fn create_xr_session(
+    xr_instance: &xr::Instance,
+    system: xr::SystemId,
+    vulkan_context: &VulkanContext,
+) -> Result<Session<Vulkan>> {
+    let (session, _, _) = unsafe {
+        xr_instance.create_session(
+            system,
+            &SessionCreateInfo {
+                instance: &vulkan_context.instance.handle() as *const _ as *const _,
+                physical_device: &vulkan_context.physical_device as *const _ as *const _,
+                device: &vulkan_context.device.handle() as *const _ as *const _,
+                queue_family_index: vulkan_context.queue_family_index,
+                queue_index: 0,
+            },
+        )
+    }?;
+
+    Ok(session)
+}
+
+fn create_xr_instance() -> Result<xr::Instance> {
+    let xr_entry = xr::Entry::linked();
+    let xr_app_info = openxr::ApplicationInfo {
+        application_name: "Hotham Cubeworld",
+        application_version: 1,
+        engine_name: "Hotham",
+        engine_version: 1,
+    };
+    let mut required_extensions = xr::ExtensionSet::default();
+    required_extensions.khr_vulkan_enable2 = true;
+    let instance = xr_entry.create_instance(&xr_app_info, &required_extensions, &[])?;
+    Ok(instance)
 }
 
 #[derive(Debug, Clone)]
--- src/swapchain.rs
+++ src/swapchain.rs
@@ -1,8 +1,14 @@
-use crate::Result;
-pub struct Swapchain {}
+use openxr::{Session, Swapchain as SwapchainHandle, Vulkan};
+
+use crate::{vulkan_context::VulkanContext, Result};
+
+pub struct Swapchain {
+    handle: SwapchainHandle<Vulkan>,
+}
 
 impl Swapchain {
-    pub fn new() -> Result<Self> {
-        Ok(Self {})
+    pub(crate) fn new(context: &VulkanContext, session: &Session<Vulkan>) -> Result<Self> {
+        let handle = todo!();
+        Ok(Self { handle })
     }
 }
