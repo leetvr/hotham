4d0b8887beb138dc14ecdf62ac74fd104dbb5035
Kane Rogers <kane.m.rogers@gmail.com>
2021-06-08 15:23:19 +1000

    XrSwapchain should be owned by App, not Swapchain

--- hotham/src/hotham_error.rs
+++ hotham/src/hotham_error.rs
@@ -1,13 +1,16 @@
+use openxr::sys::Result as OpenXRResult;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum HothamError {
-    #[error(transparent)]
-    Other(#[from] anyhow::Error),
+    #[error("There was a problem with an OpenXR operation")]
+    OpenXRError(#[from] OpenXRResult),
     #[error("The list was empty")]
     EmptyListError,
     #[error("The version of Vulkan or OpenXR is not supported")]
     UnsupportedVersionError,
     #[error("The format provided is not supported for this operation")]
     InvalidFormatError,
+    #[error(transparent)]
+    Other(#[from] anyhow::Error),
 }
---
+++ hotham/src/vertex.rs
@@ -0,0 +1,32 @@
+use ash::vk;
+use cgmath::Vector3;
+
+#[derive(Clone, Debug)]
+pub struct Vertex {
+    position: Vector3<f32>,
+    color: Vector3<f32>,
+}
+
+impl Vertex {
+    pub fn new(position: Vector3<f32>, color: Vector3<f32>) -> Self {
+        Self { position, color }
+    }
+}
+
+impl Vertex {
+    pub fn attribute_descriptions() -> Vec<vk::VertexInputAttributeDescription> {
+        let position = vk::VertexInputAttributeDescription::builder()
+            .binding(0)
+            .location(0)
+            .format(vk::Format::R32G32B32_SFLOAT)
+            .build();
+
+        let colour = vk::VertexInputAttributeDescription::builder()
+            .binding(0)
+            .location(1)
+            .format(vk::Format::R32G32B32_SFLOAT)
+            .build();
+
+        vec![position, colour]
+    }
+}
