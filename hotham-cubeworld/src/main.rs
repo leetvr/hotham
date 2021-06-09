6ef28a4c0c00ca5fa69cd4e45f4cf1c2c3e28fab
Kane Rogers <kane.m.rogers@gmail.com>
2021-06-09 17:09:53 +1000

    Change order of vertices to match cull settings

--- hotham-cubeworld/src/main.rs
+++ hotham-cubeworld/src/main.rs
@@ -16,7 +16,7 @@ impl Cubeworld {
         let v2 = Vertex::new(vec3(0.5, 0.5, 0.0), vec3(0.0, 0.0, 1.0));
         let v3 = Vertex::new(vec3(-0.5, 0.5, 0.0), vec3(1.0, 1.0, 0.0));
         let vertices = vec![v0, v1, v2, v3];
-        let indices = vec![0, 1, 2, 2, 3, 0];
+        let indices = vec![0, 2, 1, 0, 3, 2];
         Self { vertices, indices }
     }
 }
