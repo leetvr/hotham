e7929f30c0cdb43f13ef5dcf253b551d2cca27f8
Kane Rogers <kane.m.rogers@gmail.com>
2021-07-26 12:50:52 +1000

    [WIP] - Loading children

--- hotham/src/gltf_loader.rs
+++ hotham/src/gltf_loader.rs
@@ -1,19 +1,18 @@
-use std::{
-    collections::HashMap,
-    io::Cursor,
-    path::{Path, PathBuf},
-    sync::{Arc, Mutex},
-};
-
-use ash::vk;
-use cgmath::{vec2, vec3, vec4, Matrix4, Quaternion};
-use itertools::izip;
-use libktx_rs::{sources::StreamSource, RustKtxStream, TextureCreateFlags, TextureSource};
-
 use crate::{
     buffer::Buffer, mesh::Mesh, node::Node, texture::Texture, vulkan_context::VulkanContext, Vertex,
 };
 use anyhow::{anyhow, Result};
+use cgmath::{vec2, vec3, vec4, Matrix4, Quaternion};
+use std::{
+    cell::RefCell,
+    collections::HashMap,
+    io::Cursor,
+    path::{Path, PathBuf},
+    rc::Rc,
+    sync::{Arc, Mutex},
+};
+
+use ash::vk;
 
 pub(crate) fn load_gltf_nodes(
     gltf_bytes: &[u8],
@@ -28,219 +27,54 @@ pub(crate) fn load_gltf_nodes(
     let blob = data_bytes;
 
     let mut nodes = HashMap::new();
+    let root_scene = document.scenes().next().unwrap(); // safe as there is always one scene
 
-    for node_data in document.nodes() {
-        let name = node_data.name().unwrap();
-        let mut indices = Vec::new();
-        let mut positions = Vec::new();
-        let mut tex_coords = Vec::new();
-        let mut normals = Vec::new();
-        let mut tangents = Vec::new();
-        let mut normal_texture = None;
-        let mut base_color_texture = None;
-
-        // TODO: Push mesh loading into mesh struct
-        if let Some(mesh) = node_data.mesh() {
-            for primitive in mesh.primitives() {
-                let reader = primitive
-                    .reader(|buffer| Some(&blob[buffer.index()..buffer.index() + buffer.length()]));
-
-                for v in reader
-                    .read_positions()
-                    .ok_or(anyhow!("Mesh {} has no positions!"))?
-                {
-                    positions.push(vec3(v[0], v[1], v[2]));
-                }
-
-                for v in reader
-                    .read_normals()
-                    .ok_or(anyhow!("Mesh {} has no normals!"))?
-                {
-                    normals.push(vec3(v[0], v[1], v[2]));
-                }
-
-                if let Some(iter) = reader.read_tex_coords(0) {
-                    for v in iter.into_f32() {
-                        tex_coords.push(vec2(v[0], v[1]));
-                    }
-                }
-
-                if let Some(iter) = reader.read_indices() {
-                    for i in iter.into_u32() {
-                        indices.push(i);
-                    }
-                }
-
-                for t in reader
-                    .read_tangents()
-                    .ok_or(anyhow!("Mesh {} has no tangents!"))?
-                {
-                    tangents.push(vec4(t[0], t[1], t[2], t[3]));
-                }
-
-                let material = primitive.material();
-                let pbr = material
-                    .pbr_metallic_roughness()
-                    .base_color_texture()
-                    .unwrap()
-                    .texture()
-                    .source()
-                    .source();
-
-                match pbr {
-                    gltf::image::Source::Uri { uri, .. } => {
-                        let mut path = Path::new(uri).to_path_buf();
-                        path.set_extension("ktx2");
-                        let (buf, width, height) = parse_ktx(&path)?;
-                        base_color_texture =
-                            Texture::new(&vulkan_context, &buf, width, height).ok();
-                    }
-                    _ => {}
-                }
-
-                let normal = material
-                    .normal_texture()
-                    .unwrap()
-                    .texture()
-                    .source()
-                    .source();
-
-                match normal {
-                    gltf::image::Source::Uri { uri, .. } => {
-                        let mut path = Path::new(uri).to_path_buf();
-                        path.set_extension("ktx2");
-                        let (buf, width, height) = parse_ktx(&path)?;
-                        normal_texture = Texture::new(&vulkan_context, &buf, width, height).ok();
-                    }
-                    _ => {}
-                }
-            }
-        }
-
-        if base_color_texture.is_none() {
-            return Err(anyhow!("No colour texture found for {}", name));
-        }
-
-        if normal_texture.is_none() {
-            return Err(anyhow!("No normal texture found for {}", name));
-        }
-
-        let vertices: Vec<Vertex> = izip!(positions, tex_coords, normals, tangents)
-            .into_iter()
-            .map(Vertex::from_zip)
-            .collect();
-
-        println!("[HOTHAM_MODEL] {} parsed! Creating vertex buffers..", name);
-        // Create buffers
-        let vertex_buffer = Buffer::new_from_vec(
-            &vulkan_context,
-            &vertices,
-            vk::BufferUsageFlags::VERTEX_BUFFER,
-        )?;
-        let index_buffer = Buffer::new_from_vec(
-            &vulkan_context,
-            &indices,
-            vk::BufferUsageFlags::INDEX_BUFFER,
-        )?;
-        println!("[HOTHAM_MODEL] ..done!");
-
-        // Create descriptor sets
-        println!("[HOTHAM_MODEL] Creating descriptor sets for {}", name);
-        let descriptor_sets = vulkan_context.create_descriptor_sets(
-            set_layouts,
-            ubo_buffer,
-            &base_color_texture.unwrap(),
-            &normal_texture.unwrap(),
-        )?;
-        println!("[HOTHAM_MODEL] ..done!");
-
-        let transform = Matrix4::from(node_data.transform().matrix());
-
-        let mesh = Mesh {
-            vertex_buffer: vertex_buffer.handle,
-            index_buffer: index_buffer.handle,
-            descriptor_sets,
-            num_indices: indices.len() as u32,
-            transform,
-        };
-
-        let transform = node_data.transform();
-        let (translation, rotation, scale) = transform.clone().decomposed();
-
-        let node = Node {
-            parent: None,
-            children: Default::default(),
-            translation: vec3(translation[0], translation[1], translation[2]),
-            scale: vec3(scale[0], scale[1], scale[2]),
-            rotation: Quaternion::new(rotation[0], rotation[1], rotation[2], rotation[3]),
-            skin_index: node_data.skin().map(|s| s.index()),
-            matrix: Matrix4::from(transform.matrix()),
-            mesh: Some(mesh),
-        };
-
-        nodes.insert(name.to_string(), node);
+    for node_data in root_scene.nodes() {
+        let (name, node) = load_node(&node_data, blob, vulkan_context, set_layouts, ubo_buffer);
+        nodes.insert(name, node);
     }
 
     Ok(nodes)
 }
 
-pub fn parse_ktx(path: &PathBuf) -> Result<(Vec<u8>, u32, u32)> {
-    let file = get_ktx_file(path)?;
-    let stream = RustKtxStream::new(file).map_err(|e| anyhow!("Couldn't create stream: {}", e))?;
-    let source = Arc::new(Mutex::new(stream));
-    let texture = StreamSource::new(source, TextureCreateFlags::LOAD_IMAGE_DATA)
-        .create_texture()
-        .unwrap();
+fn load_node(
+    node_data: &gltf::Node,
+    blob: &[u8],
+    vulkan_context: &VulkanContext,
+    set_layouts: &[vk::DescriptorSetLayout],
+    ubo_buffer: vk::Buffer,
+) -> (String, Node) {
+    let name = node_data.name().unwrap().to_string();
+    let mesh = node_data
+        .mesh()
+        .map(|m| {
+            Mesh::load(&m, blob, vulkan_context, set_layouts, ubo_buffer)
+                .map_err(|e| eprintln!("Error loading mesh {:?}", e))
+                .ok()
+        })
+        .flatten();
+    let transform = node_data.transform();
+    let (translation, rotation, scale) = transform.clone().decomposed();
+    let children = node_data
+        .children()
+        .map(|n| {
+            let (_, node) = load_node(&n, blob, vulkan_context, set_layouts, ubo_buffer);
+            RefCell::new(Rc::new(node))
+        })
+        .collect::<Vec<_>>();
 
-    let image_buf = texture.data().to_vec();
-    let (image_height, image_width, _size) = unsafe {
-        let ktx_texture = texture.handle();
-        (
-            (*ktx_texture).baseHeight,
-            (*ktx_texture).baseWidth,
-            (*ktx_texture).dataSize,
-        )
+    let node = Node {
+        parent: None,
+        children,
+        translation: vec3(translation[0], translation[1], translation[2]),
+        scale: vec3(scale[0], scale[1], scale[2]),
+        rotation: Quaternion::new(rotation[0], rotation[1], rotation[2], rotation[3]),
+        skin_index: node_data.skin().map(|s| s.index()),
+        matrix: Matrix4::from(transform.matrix()),
+        mesh,
     };
 
-    Ok((image_buf, image_width, image_height))
-}
-
-#[cfg(target_os = "windows")]
-fn get_ktx_file(file_name: &PathBuf) -> Result<Box<std::fs::File>> {
-    use anyhow::Context;
-    use std::fs::OpenOptions;
-    let file_name = file_name
-        .to_str()
-        .ok_or(anyhow!("Unable to convert {:?} to string", file_name))?;
-    let path = format!(
-        "C:\\Users\\kanem\\Development\\hotham\\test_assets\\{}",
-        file_name
-    );
-    let path = Path::new(&path);
-    let file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .open(&path)
-        .context(format!("{:?}", path))?;
-    Ok(Box::new(file))
-}
-
-#[cfg(target_os = "android")]
-fn get_ktx_file(path: &PathBuf) -> Result<Box<Cursor<Vec<u8>>>> {
-    use std::ffi::CStr;
-    let native_activity = ndk_glue::native_activity();
-
-    let asset_manager = native_activity.asset_manager();
-    let path = path.to_str().ok_or(anyhow!("Can't parse string!"))?;
-    let path_with_nul = format!("{}\0", path);
-    let path = unsafe { CStr::from_bytes_with_nul_unchecked(path_with_nul.as_bytes()) };
-
-    let mut asset = asset_manager
-        .open(path)
-        .ok_or(anyhow!("Can't open: {:?}", path))?;
-
-    // delicious
-    Ok(Box::new(Cursor::new(asset.get_buffer()?.to_vec())))
+    (name, node)
 }
 
 #[cfg(test)]
@@ -268,8 +102,7 @@ mod tests {
         let buffer = vk::Buffer::null();
         let nodes = load_gltf_nodes(&gltf, data, &vulkan_context, &[set_layout], buffer).unwrap();
         assert!(nodes.len() == 1);
-        assert!(nodes.get("hand").is_some());
-        let hand = nodes.get("hand").unwrap();
+        let hand = nodes.get("Hand").unwrap();
         assert!(hand.children.len() > 1);
     }
 }
