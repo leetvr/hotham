a9c72f536245e2d9934c820f232cbb61363c4b05
Kane Rogers <kane.m.rogers@gmail.com>
2021-08-24 09:59:40 +1000

    Fix hotham-simulator build script

--- hotham-simulator/build.rs
+++ hotham-simulator/build.rs
@@ -4,19 +4,14 @@ use shaderc::{Compiler, ShaderKind};
 
 #[cfg(target_os = "windows")]
 fn main() {
-    println!("cargo:rerun-if-changed=src/shaders/*.frag");
-    println!("cargo:rerun-if-changed=src/shaders/*.vert");
-    println!("cargo:rerun-if-changed=wrapper.h");
+    println!("cargo:rerun-if-changed=build_input");
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
-        .header("wrapper.h")
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .header("build_input/wrapper.h")
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
@@ -28,33 +23,33 @@ fn main() {
 
     let mut compiler = Compiler::new().expect("Unable to instantiate compiler");
 
-    let mut file = Path::new("./src/shaders/viewdisplay.vert").to_path_buf();
-    let source_text = fs::read(&file).expect("Unable to read string");
+    let input_file = Path::new("./build_input/viewdisplay.vert").to_path_buf();
+    let source_text = fs::read(&input_file).expect("Unable to read string");
     let source_text = str::from_utf8(&source_text).expect("Unable to parse string");
     let artifact = compiler
         .compile_into_spirv(
             source_text,
             ShaderKind::Vertex,
-            file.file_name().unwrap().to_str().unwrap(),
+            input_file.file_name().unwrap().to_str().unwrap(),
             "main",
             None,
         )
         .expect("Unable to compile file");
-    file.set_extension("vert.spv");
-    fs::write(&file, &artifact.as_binary_u8()).expect("Unable to write spirv to file");
+    let output_file = Path::new("./src/shaders/viewdisplay.vert.spv").to_path_buf();
+    fs::write(&output_file, &artifact.as_binary_u8()).expect("Unable to write spirv to file");
 
-    let mut file = Path::new("./src/shaders/viewdisplay.frag").to_path_buf();
-    let source_text = fs::read(&file).expect("Unable to read string");
+    let input_file = Path::new("./build_input/viewdisplay.frag").to_path_buf();
+    let source_text = fs::read(&input_file).expect("Unable to read string");
     let source_text = str::from_utf8(&source_text).expect("Unable to parse string");
     let artifact = compiler
         .compile_into_spirv(
             source_text,
             ShaderKind::Fragment,
-            file.file_name().unwrap().to_str().unwrap(),
+            input_file.file_name().unwrap().to_str().unwrap(),
             "main",
             None,
         )
         .expect("Unable to compile file");
-    file.set_extension("frag.spv");
-    fs::write(&file, &artifact.as_binary_u8()).expect("Unable to write spirv to file");
+    let output_file = Path::new("./src/shaders/viewdisplay.frag.spv").to_path_buf();
+    fs::write(&output_file, &artifact.as_binary_u8()).expect("Unable to write spirv to file");
 }
