0507aa1731aeb7810097874630fc9e373f4a3275
Kane Rogers <kane.m.rogers@gmail.com>
2022-02-11 16:52:20 +1100

    Add sounds

--- hotham/src/components/skin.rs
+++ hotham/src/components/skin.rs
@@ -1,5 +1,7 @@
-// Empty struct, used as a "tag" to indicate this mesh has a skin.
+/// Component added to an entity to point to the joints in the node
+/// Automatically added by `gltf_loader`
 #[derive(Debug, Clone, PartialEq)]
 pub struct Skin {
+    /// List of joints, represented by their node ID
     pub joint_ids: Vec<usize>,
 }
--- hotham/src/components/sound_emitter.rs
+++ hotham/src/components/sound_emitter.rs
@@ -4,9 +4,14 @@ use oddio::{Frames, Stop};
 
 type AudioHandle = oddio::Handle<oddio::SpatialBuffered<oddio::Stop<oddio::FramesSignal<f32>>>>;
 
+/// A component added to an entity to allow it to emit a sound, usually a sound effect
+/// Used by `audio_system`
 pub struct SoundEmitter {
+    /// The actual sound data
     pub frames: Arc<Frames<f32>>,
+    /// Handle into the `oddio` spatialiser
     pub handle: Option<AudioHandle>,
+    /// Used to indicate that the emitter wants to change its state
     pub next_state: Option<SoundState>,
 }
 
@@ -20,14 +25,19 @@ impl Clone for SoundEmitter {
     }
 }
 
+/// State of a sound
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum SoundState {
+    /// The sound has stopped permenantly
     Stopped,
+    /// The sound is playing
     Playing,
+    /// The sound is temporarily paused
     Paused,
 }
 
 impl SoundEmitter {
+    /// Convenience function to create a new `SoundEmitter`
     pub fn new(frames: Arc<Frames<f32>>) -> Self {
         Self {
             frames,
@@ -36,6 +46,7 @@ impl SoundEmitter {
         }
     }
 
+    /// Convenience function to get the `SoundState` of this `SoundEmitter`
     pub fn current_state(&mut self) -> SoundState {
         if let Some(handle) = self.handle.as_mut() {
             let control = handle.control::<Stop<_>, _>();
@@ -51,18 +62,22 @@ impl SoundEmitter {
         }
     }
 
+    /// Play the sound
     pub fn play(&mut self) {
         self.next_state = Some(SoundState::Playing);
     }
 
+    /// Pause the sound
     pub fn pause(&mut self) {
         self.next_state = Some(SoundState::Paused);
     }
 
+    /// Stop the sound
     pub fn stop(&mut self) {
         self.next_state = Some(SoundState::Stopped);
     }
 
+    /// Resume the sound
     pub fn resume(&mut self) {
         self.next_state = Some(SoundState::Playing);
     }
