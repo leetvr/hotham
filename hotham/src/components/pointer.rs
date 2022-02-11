4173deebaee0a7bb2cde41456b8885d2f6f0337a
Kane Rogers <kane.m.rogers@gmail.com>
2022-02-11 16:44:39 +1100

    Document parent, pointer

--- hotham/src/components/pointer.rs
+++ hotham/src/components/pointer.rs
@@ -1,6 +1,10 @@
 use super::hand::Handedness;
 
+/// A component added to an entity to allow users to interact with `Panels` using their
+/// controllers.
 pub struct Pointer {
+    /// Which hand is the pointer in?
     pub handedness: Handedness,
+    /// How much has the trigger been pulled down?
     pub trigger_value: f32,
 }
