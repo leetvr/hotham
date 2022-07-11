255edf8bf00bb5c9488e04421f4d0e395a2c03d1
Kane Rogers <kane.m.rogers@gmail.com>
2022-07-11 23:41:00 +1000

    [Bug]: Builds fail to link stdc++ on linux Fixes #250

--- hotham/build.rs
+++ hotham/build.rs
@@ -1,5 +1,11 @@
 pub fn main() {
     // On Android, we must ensure that we're dynamically linking against the C++ standard library.
     // For more details, see https://github.com/rust-windowing/android-ndk-rs/issues/167
-    println!("cargo:rustc-link-lib=dylib=c++");
+    use std::env::var;
+    if var("TARGET")
+        .map(|target| target == "aarch64-linux-android")
+        .unwrap_or(false)
+    {
+        println!("cargo:rustc-link-lib=dylib=c++");
+    }
 }
