fb8a9b2121b1bf469a3026db477ccaa0f463825d
Rasmus Brönnegård <1162652+rasmusgo@users.noreply.github.com>
2022-07-24 21:53:01 +0200

    hotham-simulator: Handle all pending events in update_camera
    This makes a big difference when using a mouse with high refresh rate.

--- hotham-simulator/src/state.rs
+++ hotham-simulator/src/state.rs
@@ -218,7 +218,7 @@ impl State {
         let movement_speed = 2f32 * dt;
         let mouse_sensitivity = 4f32 * dt;
 
-        if let Ok(input_event) = self.event_rx.as_ref()?.try_recv() {
+        while let Ok(input_event) = self.event_rx.as_ref()?.try_recv() {
             match input_event {
                 DeviceEvent::Key(keyboard_input) => self.input_state.process_event(keyboard_input),
                 DeviceEvent::MouseMotion { delta: (x, y) } => {
