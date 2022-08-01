2247519cc18890864171bdb3bf264fc9c2c0b96d
Rasmus Brönnegård <1162652+rasmusgo@users.noreply.github.com>
2022-08-01 21:57:21 +0200

    Fix comment in components/parent.rs

--- hotham/src/components/parent.rs
+++ hotham/src/components/parent.rs
@@ -1,6 +1,6 @@
 use hecs::Entity;
 
 /// Component added to indicate that an entity has a parent
-/// Used by `update_parent_transform_matrix_system`
+/// Used by `update_global_transform_with_parent_system`
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Parent(pub Entity);
