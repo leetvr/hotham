94cb9ac40cd505fa395ab17e84f1454476598477
Rasmus Brönnegård <1162652+rasmusgo@users.noreply.github.com>
2022-08-10 23:32:30 +0200

    Improve ergonomics of debug_system
    Reset values by pressing both buttons.
    Fix floating point issues with IBL factor.
    Turn off IBL with a-button.

--- hotham/src/systems/debug.rs
+++ hotham/src/systems/debug.rs
@@ -2,23 +2,41 @@ use crate::resources::{InputContext, RenderContext};
 
 /// A simple system used to assist with debugging the fragment shader.
 pub fn debug_system(input_context: &InputContext, render_context: &mut RenderContext) {
-    if input_context.x_button_just_pressed() {
-        let params = &mut render_context.scene_data.params;
-        params.w = 0.;
-        params.z = ((params.z + 1.) % 7.) as f32;
-        println!("[HOTHAM_DEBUG] params.z is now {}", params.z);
-    }
-
-    if input_context.y_button_just_pressed() {
-        let params = &mut render_context.scene_data.params;
+    let params = &mut render_context.scene_data.params;
+    if (input_context.x_button_just_pressed() || input_context.y_button_just_pressed())
+        && input_context.x_button()
+        && input_context.y_button()
+    {
         params.z = 0.;
-        params.w = ((params.w + 1.) % 6.) as f32;
-        println!("[HOTHAM_DEBUG] params.w is now {}", params.w);
+        params.w = 0.;
+        println!("[HOTHAM_DEBUG] params.z and params.w are now 0");
+    } else {
+        if input_context.x_button_just_pressed() {
+            params.w = 0.;
+            params.z = ((params.z + 1.) % 7.) as f32;
+            println!("[HOTHAM_DEBUG] params.z is now {}", params.z);
+        }
+        if input_context.y_button_just_pressed() {
+            params.z = 0.;
+            params.w = ((params.w + 1.) % 6.) as f32;
+            println!("[HOTHAM_DEBUG] params.w is now {}", params.w);
+        }
     }
 
-    if input_context.b_button_just_pressed() {
-        let params = &mut render_context.scene_data.params;
-        params.x = ((params.x + 0.1) % 5.) as f32;
+    if (input_context.a_button_just_pressed() || input_context.b_button_just_pressed())
+        && input_context.a_button()
+        && input_context.b_button()
+    {
+        params.x = 1.;
         println!("[HOTHAM_DEBUG] params.x is now {}", params.x);
+    } else {
+        if input_context.b_button_just_pressed() {
+            params.x = (params.x * 10. + 1.).round() % 50. * 0.1 as f32;
+            println!("[HOTHAM_DEBUG] params.x is now {}", params.x);
+        }
+        if input_context.a_button_just_pressed() {
+            params.x = 0.;
+            println!("[HOTHAM_DEBUG] params.x is now {}", params.x);
+        }
     }
 }
