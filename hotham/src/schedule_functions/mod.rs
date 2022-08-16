469fad38c414f5b55f3ef432839c1428f4dc6ccc
MultisampledNight <contact@multisamplednight.com>
2022-08-16 12:09:25 +0200

    Put hotham-debug-server behind optional feature
    Called debug_server.

--- hotham/src/schedule_functions/mod.rs
+++ hotham/src/schedule_functions/mod.rs
@@ -1,8 +1,10 @@
 #![allow(missing_docs)]
 pub mod apply_haptic_feedback;
 pub mod physics_step;
+#[cfg(feature = "debug_server")]
 pub mod sync_debug_server;
 
 pub use apply_haptic_feedback::apply_haptic_feedback;
 pub use physics_step::physics_step;
+#[cfg(feature = "debug_server")]
 pub use sync_debug_server::sync_debug_server;
