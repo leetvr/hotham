3f9b42cd3ccbe704cb62538a5f767b90f1e56295
Rasmus Brönnegård <1162652+rasmusgo@users.noreply.github.com>
2022-10-25 21:20:20 +0200

    Add custom rendering example as duplicate of complex scene

---
+++ examples/custom-rendering/src/lib.rs
@@ -0,0 +1,106 @@
+pub mod navigation;
+
+use hotham::{
+    asset_importer::{self, add_model_to_world},
+    components::{hand::Handedness, physics::SharedShape, Collider, Grabbable, LocalTransform},
+    glam,
+    hecs::World,
+    systems::{
+        animation_system, debug::debug_system, grabbing_system, hands::add_hand, hands_system,
+        physics_system, rendering::rendering_system, skinning::skinning_system,
+        update_global_transform_system, update_global_transform_with_parent_system,
+    },
+    xr, Engine, HothamResult, TickData,
+};
+use navigation::navigation_system;
+
+#[derive(Clone, Debug, Default)]
+/// The state is used for manipulating the stage transform
+pub struct State {
+    global_from_left_grip: Option<glam::Affine3A>,
+    global_from_right_grip: Option<glam::Affine3A>,
+    scale: Option<f32>,
+}
+
+#[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on"))]
+pub fn main() {
+    println!("[HOTHAM_CUSTOM_RENDERING_EXAMPLE] MAIN!");
+    real_main().expect("Error running app!");
+    println!("[HOTHAM_CUSTOM_RENDERING_EXAMPLE] FINISHED! Goodbye!");
+}
+
+pub fn real_main() -> HothamResult<()> {
+    let mut engine = Engine::new();
+    let mut state = Default::default();
+    init(&mut engine)?;
+
+    while let Ok(tick_data) = engine.update() {
+        tick(tick_data, &mut engine, &mut state);
+        engine.finish()?;
+    }
+
+    Ok(())
+}
+
+fn tick(tick_data: TickData, engine: &mut Engine, state: &mut State) {
+    if tick_data.current_state == xr::SessionState::FOCUSED {
+        hands_system(engine);
+        grabbing_system(engine);
+        physics_system(engine);
+        animation_system(engine);
+        navigation_system(engine, state);
+        update_global_transform_system(engine);
+        update_global_transform_with_parent_system(engine);
+        skinning_system(engine);
+        debug_system(engine);
+    }
+
+    rendering_system(engine, tick_data.swapchain_image_index);
+}
+
+fn init(engine: &mut Engine) -> Result<(), hotham::HothamError> {
+    let render_context = &mut engine.render_context;
+    let vulkan_context = &mut engine.vulkan_context;
+    let world = &mut engine.world;
+
+    let mut glb_buffers: Vec<&[u8]> = vec![
+        include_bytes!("../../../test_assets/left_hand.glb"),
+        include_bytes!("../../../test_assets/right_hand.glb"),
+    ];
+
+    #[cfg(target_os = "android")]
+    glb_buffers.push(include_bytes!(
+        "../../../test_assets/damaged_helmet_squished.glb"
+    ));
+
+    #[cfg(not(target_os = "android"))]
+    glb_buffers.push(include_bytes!("../../../test_assets/damaged_helmet.glb"));
+
+    let models =
+        asset_importer::load_models_from_glb(&glb_buffers, vulkan_context, render_context)?;
+    add_helmet(&models, world, [-1., 1.4, -1.].into());
+    add_helmet(&models, world, [1., 1.4, -1.].into());
+    add_hand(&models, Handedness::Left, world);
+    add_hand(&models, Handedness::Right, world);
+
+    Ok(())
+}
+
+fn add_helmet(
+    models: &std::collections::HashMap<String, World>,
+    world: &mut World,
+    translation: glam::Vec3,
+) {
+    let helmet = add_model_to_world("Damaged Helmet", models, world, None)
+        .expect("Could not find Damaged Helmet");
+
+    {
+        let mut local_transform = world.get::<&mut LocalTransform>(helmet).unwrap();
+        local_transform.translation = translation;
+        local_transform.scale = [0.5, 0.5, 0.5].into();
+    }
+
+    let collider = Collider::new(SharedShape::ball(0.35));
+
+    world.insert(helmet, (collider, Grabbable {})).unwrap();
+}
---
+++ examples/custom-rendering/src/navigation.rs
@@ -0,0 +1,124 @@
+use hotham::{
+    components::{Collider, Hand, LocalTransform},
+    contexts::InputContext,
+    glam::{Affine3A, Vec3},
+    hecs::{self, World},
+    Engine,
+};
+
+use crate::State;
+
+/// Navigation system
+/// Allows the player to navigate by grabbing empty space with their hands.
+pub fn navigation_system(engine: &mut Engine, state: &mut State) {
+    let world = &mut engine.world;
+    let input_context = &mut engine.input_context;
+    navigation_system_inner(world, input_context, engine.stage_entity, state);
+}
+
+fn navigation_system_inner(
+    world: &mut World,
+    input_context: &mut InputContext,
+    stage_entity: hecs::Entity,
+    state: &mut State,
+) {
+    // First, check to see if either of the hands have collided with anything.
+    let hands_have_collisions = world
+        .query::<&Collider>()
+        .with::<&Hand>()
+        .iter()
+        .any(|(_, collider)| !collider.collisions_this_frame.is_empty());
+
+    // If they have, then just return.
+    if hands_have_collisions {
+        return;
+    }
+
+    // Get the stage transform.
+    let mut stage_transform = world.get::<&mut LocalTransform>(stage_entity).unwrap();
+    let global_from_stage = stage_transform.to_affine();
+
+    // Get the hand transforms.
+    let stage_from_left_grip = input_context.left.stage_from_grip();
+    let stage_from_right_grip = input_context.right.stage_from_grip();
+
+    // Update grip states.
+    if input_context.left.grip_button_just_pressed() {
+        state.global_from_left_grip = Some(global_from_stage * stage_from_left_grip);
+    }
+    if input_context.right.grip_button_just_pressed() {
+        state.global_from_right_grip = Some(global_from_stage * stage_from_right_grip);
+    }
+    if input_context.right.grip_button() && input_context.left.grip_button_just_released() {
+        // Handle when going from two grips to one
+        state.global_from_right_grip = Some(global_from_stage * stage_from_right_grip);
+    }
+    if !input_context.left.grip_button() {
+        state.global_from_left_grip = None;
+        state.scale = None;
+    }
+    if !input_context.right.grip_button() {
+        state.global_from_right_grip = None;
+    }
+
+    // Adjust global_from_stage so that global_from_grip stays fixed.
+    // global_from_stage * stage_from_grip = global_from_stored_grip
+    // global_from_stage = global_from_stored_grip * grip_from_stage
+    match (
+        state.global_from_left_grip,
+        state.global_from_right_grip,
+        state.scale,
+    ) {
+        (Some(global_from_stored_left_grip), None, None) => {
+            stage_transform.update_from_affine(
+                &(global_from_stored_left_grip * stage_from_left_grip.inverse()),
+            );
+        }
+        (Some(global_from_stored_left_grip), None, Some(scale)) => {
+            stage_transform.update_from_affine(
+                &(global_from_stored_left_grip
+                    * Affine3A::from_scale(Vec3::new(scale, scale, scale))
+                    * stage_from_left_grip.inverse()),
+            );
+        }
+        (None, Some(global_from_stored_right_grip), _) => {
+            stage_transform.update_from_affine(
+                &(global_from_stored_right_grip * stage_from_right_grip.inverse()),
+            );
+        }
+        (Some(global_from_stored_left_grip), Some(global_from_stored_right_grip), _) => {
+            // Gripping with both hands allows scaling the scene
+            // The first hand acts as an anchor and the second hand only scales the scene.
+            let stored_left_grip_in_global = global_from_stored_left_grip.translation;
+            let stored_right_grip_in_global = global_from_stored_right_grip.translation;
+            let left_grip_in_stage = stage_from_left_grip.translation;
+            let right_grip_in_stage = stage_from_right_grip.translation;
+
+            let unscaled_global_from_stage =
+                global_from_stored_left_grip * stage_from_left_grip.inverse();
+            let left_grip_in_unscaled_global =
+                unscaled_global_from_stage.transform_point3a(left_grip_in_stage);
+            let right_grip_in_unscaled_global =
+                unscaled_global_from_stage.transform_point3a(right_grip_in_stage);
+            let stored_dist_in_global =
+                stored_left_grip_in_global.distance(stored_right_grip_in_global);
+            let dist_in_unscaled_global =
+                left_grip_in_unscaled_global.distance(right_grip_in_unscaled_global);
+            let scale = stored_dist_in_global / dist_in_unscaled_global;
+
+            // Remember scale for when one grip gets released.
+            state.scale = Some(scale);
+
+            // Let left hand be dominant for now.
+            let stored_left_grip_from_left_grip =
+                Affine3A::from_scale(Vec3::new(scale, scale, scale));
+
+            stage_transform.update_from_affine(
+                &(global_from_stored_left_grip
+                    * stored_left_grip_from_left_grip
+                    * stage_from_left_grip.inverse()),
+            );
+        }
+        (None, None, _) => (),
+    };
+}
