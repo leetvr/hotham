45b09166824bd3d9f62956d900db14d9e6ba454c
Kane Rogers <kane.m.rogers@gmail.com>
2022-12-02 18:10:51 +1100

    WIP

--- hotham/src/rendering/swapchain.rs
+++ hotham/src/rendering/swapchain.rs
@@ -17,7 +17,7 @@ pub struct SwapchainInfo {
     /// The images held in the swapchain
     pub images: Vec<vk::Image>,
     /// Optional images used for fixed foveated rendering
-    pub ffr_image: Option<FFRImage>,
+    pub ffr_images: Option<Vec<FFRImage>>,
 }
 
 impl SwapchainInfo {
@@ -31,12 +31,12 @@ impl SwapchainInfo {
         //     .map(vk::Image::from_raw)
         //     .collect::<Vec<_>>();
 
-        let (images, ffr_image) = get_ffr_image(handle);
+        let (images, ffr_images) = get_swapchain_images(handle);
 
         Ok(Self {
             resolution,
             images,
-            ffr_image,
+            ffr_images,
         })
     }
 }
@@ -50,7 +50,9 @@ pub struct FFRImage {
     pub image: vk::Image,
 }
 
-fn get_ffr_image(handle: &SwapchainHandle<Vulkan>) -> (Vec<vk::Image>, Option<FFRImage>) {
+fn get_swapchain_images(
+    handle: &SwapchainHandle<Vulkan>,
+) -> (Vec<vk::Image>, Option<Vec<FFRImage>>) {
     let fp = handle.instance().fp();
     let mut output = 0;
 
@@ -63,15 +65,19 @@ fn get_ffr_image(handle: &SwapchainHandle<Vulkan>) -> (Vec<vk::Image>, Option<FF
         }
 
         let mut swapchain_foveation_vulkan =
-            openxr::sys::SwapchainImageFoveationVulkanFB::out(std::ptr::null_mut() as _);
-        let mut buf = vec![
-            openxr::sys::SwapchainImageVulkanKHR {
+            vec![
+                openxr::sys::SwapchainImageFoveationVulkanFB::out(std::ptr::null_mut() as _);
+                output as _
+            ];
+
+        let mut buf = swapchain_foveation_vulkan
+            .iter_mut()
+            .map(|s| openxr::sys::SwapchainImageVulkanKHR {
                 ty: openxr::sys::SwapchainImageVulkanKHR::TYPE,
-                next: &mut swapchain_foveation_vulkan as *mut _ as *mut std::ffi::c_void,
+                next: s as *mut _ as *mut _,
                 image: 0,
-            };
-            output as _
-        ];
+            })
+            .collect::<Vec<_>>();
 
         let result = (fp.enumerate_swapchain_images)(
             handle.as_raw(),
@@ -87,24 +93,29 @@ fn get_ffr_image(handle: &SwapchainHandle<Vulkan>) -> (Vec<vk::Image>, Option<FF
         buf
     };
 
-    println!("Got images: {images:?}");
+    let ffr_images = images
+        .iter()
+        .map(|i| {
+            let ffr_image =
+                unsafe { *(i.next as *mut openxr::sys::SwapchainImageFoveationVulkanFB) };
+            FFRImage {
+                resolution: vk::Extent2D {
+                    width: ffr_image.width,
+                    height: ffr_image.height,
+                },
+                image: vk::Image::from_raw(ffr_image.image),
+            }
+        })
+        .collect();
 
-    let ffr_image =
-        unsafe { *(images[0].next as *mut openxr::sys::SwapchainImageFoveationVulkanFB) };
-    let ffr_image = FFRImage {
-        resolution: vk::Extent2D {
-            width: ffr_image.width,
-            height: ffr_image.height,
-        },
-        image: vk::Image::from_raw(ffr_image.image),
-    };
+    println!("FFR images: {ffr_images:?}");
 
     let images = images
         .iter()
         .map(|i| vk::Image::from_raw(i.image))
         .collect();
 
-    (images, Some(ffr_image))
+    (images, Some(ffr_images))
 }
 
 /// A thin container for OpenXR to pass the details of its Swapchain to RenderContext.
@@ -151,51 +162,146 @@ impl Swapchain {
             .unwrap();
 
         // Framebuffers, used for rendering the final image to the swapchain.
-        let framebuffers = swapchain_info
-            .images
-            .iter()
-            .flat_map(|i| {
-                vulkan_context.create_image_view(
-                    i,
-                    COLOR_FORMAT,
-                    vk::ImageViewType::TYPE_2D_ARRAY,
-                    2,
-                    1,
-                    DEFAULT_COMPONENT_MAPPING,
-                )
-            })
-            .map(|swapchain_image_view| {
-                let msaa_attachments = [color_image.view, depth_image.view, swapchain_image_view];
-                let non_msaa_attachments = [swapchain_image_view, depth_image.view];
-
-                let frame_buffer_create_info = if USE_MSAA {
-                    vk::FramebufferCreateInfo::builder()
-                        .render_pass(render_pass)
-                        .attachments(&msaa_attachments)
-                        .width(swapchain_info.resolution.width)
-                        .height(swapchain_info.resolution.height)
-                        .layers(1) // NOTE: multiview takes care of layers.
-                } else {
-                    vk::FramebufferCreateInfo::builder()
-                        .render_pass(render_pass)
-                        .attachments(&non_msaa_attachments)
-                        .width(swapchain_info.resolution.width)
-                        .height(swapchain_info.resolution.height)
-                        .layers(1) // NOTE: multiview takes care of layers.
-                };
-
-                unsafe {
-                    vulkan_context
-                        .device
-                        .create_framebuffer(&frame_buffer_create_info, None)
-                }
-                .unwrap()
-            })
-            .collect();
+        let framebuffers = if swapchain_info.ffr_images.is_some() {
+            create_framebuffers_with_ffr(
+                swapchain_info,
+                vulkan_context,
+                color_image,
+                depth_image,
+                render_pass,
+            )
+        } else {
+            create_framebuffers(
+                swapchain_info,
+                vulkan_context,
+                color_image,
+                depth_image,
+                render_pass,
+            )
+        };
 
         Self {
             render_area,
             framebuffers,
         }
     }
+}
+
+fn create_framebuffers_with_ffr(
+    swapchain_info: &SwapchainInfo,
+    vulkan_context: &VulkanContext,
+    color_image: super::image::Image,
+    depth_image: super::image::Image,
+    render_pass: vk::RenderPass,
+) -> Vec<vk::Framebuffer> {
+    let ffr_image_view = vulkan_context
+        .create_image_view(
+            &swapchain_info.ffr_images.as_ref().unwrap()[0].image,
+            vk::Format::R8G8_UNORM,
+            vk::ImageViewType::TYPE_2D_ARRAY,
+            2,
+            1,
+            DEFAULT_COMPONENT_MAPPING,
+        )
+        .unwrap();
+    let framebuffers = swapchain_info
+        .images
+        .iter()
+        .map(|swapchain_image| {
+            vulkan_context
+                .create_image_view(
+                    swapchain_image,
+                    COLOR_FORMAT,
+                    vk::ImageViewType::TYPE_2D_ARRAY,
+                    2,
+                    1,
+                    DEFAULT_COMPONENT_MAPPING,
+                )
+                .unwrap()
+        })
+        .map(|swapchain_image_view| {
+            let msaa_attachments = [
+                color_image.view,
+                depth_image.view,
+                ffr_image_view,
+                swapchain_image_view,
+            ];
+            let non_msaa_attachments = [swapchain_image_view, depth_image.view, ffr_image_view];
+
+            let frame_buffer_create_info = if USE_MSAA {
+                vk::FramebufferCreateInfo::builder()
+                    .render_pass(render_pass)
+                    .attachments(&msaa_attachments)
+                    .width(swapchain_info.resolution.width)
+                    .height(swapchain_info.resolution.height)
+                    .layers(1) // NOTE: multiview takes care of layers.
+            } else {
+                vk::FramebufferCreateInfo::builder()
+                    .render_pass(render_pass)
+                    .attachments(&non_msaa_attachments)
+                    .width(swapchain_info.resolution.width)
+                    .height(swapchain_info.resolution.height)
+                    .layers(1) // NOTE: multiview takes care of layers.
+            };
+
+            unsafe {
+                vulkan_context
+                    .device
+                    .create_framebuffer(&frame_buffer_create_info, None)
+            }
+            .unwrap()
+        })
+        .collect();
+    framebuffers
+}
+
+fn create_framebuffers(
+    swapchain_info: &SwapchainInfo,
+    vulkan_context: &VulkanContext,
+    color_image: super::image::Image,
+    depth_image: super::image::Image,
+    render_pass: vk::RenderPass,
+) -> Vec<vk::Framebuffer> {
+    let framebuffers = swapchain_info
+        .images
+        .iter()
+        .flat_map(|i| {
+            vulkan_context.create_image_view(
+                i,
+                COLOR_FORMAT,
+                vk::ImageViewType::TYPE_2D_ARRAY,
+                2,
+                1,
+                DEFAULT_COMPONENT_MAPPING,
+            )
+        })
+        .map(|swapchain_image_view| {
+            let msaa_attachments = [color_image.view, depth_image.view, swapchain_image_view];
+            let non_msaa_attachments = [swapchain_image_view, depth_image.view];
+
+            let frame_buffer_create_info = if USE_MSAA {
+                vk::FramebufferCreateInfo::builder()
+                    .render_pass(render_pass)
+                    .attachments(&msaa_attachments)
+                    .width(swapchain_info.resolution.width)
+                    .height(swapchain_info.resolution.height)
+                    .layers(1) // NOTE: multiview takes care of layers.
+            } else {
+                vk::FramebufferCreateInfo::builder()
+                    .render_pass(render_pass)
+                    .attachments(&non_msaa_attachments)
+                    .width(swapchain_info.resolution.width)
+                    .height(swapchain_info.resolution.height)
+                    .layers(1) // NOTE: multiview takes care of layers.
+            };
+
+            unsafe {
+                vulkan_context
+                    .device
+                    .create_framebuffer(&frame_buffer_create_info, None)
+            }
+            .unwrap()
+        })
+        .collect();
+    framebuffers
 }
