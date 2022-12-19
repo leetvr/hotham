6743fe28b40dde8585bd0b3659d5cbeb950c071e
Kane Rogers <kane.m.rogers@gmail.com>
2022-12-19 23:41:09 +0100

    WIP - filament f16 shaders

--- hotham/src/shaders/brdf.glsl
+++ hotham/src/shaders/brdf.glsl
@@ -1,59 +1,69 @@
-// Fresnel
-//
-// http://graphicrants.blogspot.com/2013/08/specular-brdf-reference.html
-// https://github.com/wdas/brdf/tree/master/src/brdfs
-// https://google.github.io/filament/Filament.md.html
-//
+// Filament's lambertian term is 1.0 / PI, so we just pre-compute that
+#define BRDF_LAMBERTIAN F16(0.3183098862)
 
 // The following equation models the Fresnel reflectance term of the spec equation (aka F())
 // Implementation of fresnel from [4], Equation 15
-const float M_PI = 3.141592653589793;
 
 // Anything less than 2% is physically impossible and is instead considered to be shadowing. Compare to "Real-Time-Rendering" 4th edition on page 325.
-const vec3 f90 = vec3(1.0);
+const f16vec3 f90 = V16(1.0);
 
-vec3 F_Schlick(vec3 f0, float VdotH) {
-    return f0 + (f90 - f0) * pow(clamp(1.0 - VdotH, 0.0, 1.0), 5.0);
-}
+// f16vec3 F_Schlick(f16vec3 f0, float16_tVdotH) {
+//     return f0 + (f90 - f0) * pow(clamp(1.0 - VdotH, 0.0, 1.0), 5.0);
+// }
 
 // Smith Joint GGX
 // Note: Vis = G / (4 * NdotL * NdotV)
 // see Eric Heitz. 2014. Understanding the Masking-Shadowing Function in Microfacet-Based BRDFs. Journal of Computer Graphics Techniques, 3
 // see Real-Time Rendering. Page 331 to 336.
 // see https://google.github.io/filament/Filament.md.html#materialsystem/specularbrdf/geometricshadowing(specularg)
-float V_GGX(float NdotL, float NdotV, float alphaRoughness) {
-    float alphaRoughnessSq = alphaRoughness * alphaRoughness;
+// float16_t V_GGX(float16_t NdotL, float16_t NdotV, float16_t alphaRoughness) {
+//     float16_t alphaRoughnessSq = alphaRoughness * alphaRoughness;
 
-    float GGXV = NdotL * sqrt(NdotV * NdotV * (1.0 - alphaRoughnessSq) + alphaRoughnessSq);
-    float GGXL = NdotV * sqrt(NdotL * NdotL * (1.0 - alphaRoughnessSq) + alphaRoughnessSq);
+//     float16_t GGXV = NdotL * sqrt(NdotV * NdotV * (1.0 - alphaRoughnessSq) + alphaRoughnessSq);
+//     float16_t GGXL = NdotV * sqrt(NdotL * NdotL * (1.0 - alphaRoughnessSq) + alphaRoughnessSq);
 
-    float GGX = GGXV + GGXL;
-    if (GGX > 0.0) {
-        return 0.5 / GGX;
-    }
-    return 0.0;
+//     float16_t GGX = GGXV + GGXL;
+//     if (GGX > 0.0) {
+//         return 0.5 / GGX;
+//     }
+//     return 0.0;
+// }
+
+float16_t D_GGX(float16_t roughness, f16vec3 n, float16_t NdotH, f16vec3 h) {
+    f16vec3 NxH = cross(n, h);
+    float16_t oneMinusNoHSquared = dot(NxH, NxH);
+
+    float16_t a = NdotH * roughness;
+    float16_t k = roughness / (oneMinusNoHSquared + a * a);
+    float16_t d = k * k * BRDF_LAMBERTIAN;
+    return saturateMediump(d);
 }
 
-// The following equation(s) model the distribution of microfacet normals across the area being drawn (aka D())
-// Implementation from "Average Irregularity Representation of a Roughened Surface for Ray Reflection" by T. S. Trowbridge, and K. P. Reitz
-// Follows the distribution function recommended in the SIGGRAPH 2013 course notes from EPIC Games [1], Equation 3.
-float D_GGX(float NdotH, float alphaRoughness) {
-    float alphaRoughnessSq = alphaRoughness * alphaRoughness;
-    float f = (NdotH * NdotH) * (alphaRoughnessSq - 1.0) + 1.0;
-    return alphaRoughnessSq / (M_PI * f * f);
+float16_t V_SmithGGXCorrelated_Fast(float16_t roughness, float16_t NdotV, float16_t NdotL) {
+    // Hammon 2017, "PBR Diffuse Lighting for GGX+Smith Microsurfaces"
+    float16_t v = F16(0.5) / mix(F16(2.0) * NdotL * NdotV, NdotL + NdotV, roughness);
+    return saturateMediump(v);
 }
 
-//https://github.com/KhronosGroup/glTF/tree/master/specification/2.0#acknowledgments AppendixB
-vec3 BRDF_lambertian(vec3 f0, vec3 diffuseColor, float VdotH) {
-    // see https://seblagarde.wordpress.com/2012/01/08/pi-or-not-to-pi-in-game-lighting-equation/
-    return (1.0 - F_Schlick(f0, VdotH)) * (diffuseColor / M_PI);
+// KR: why is this VdotH instead of LdotH?
+f16vec3 F_Schlick(const f16vec3 f0, float16_t VdotH) {
+    float16_t f = pow(F16(1.0) - VdotH, F16(5.0));
+    return f + f0 * (F16(1.0) - f);
 }
 
-//  https://github.com/KhronosGroup/glTF/tree/master/specification/2.0#acknowledgments AppendixB
-vec3 BRDF_specularGGX(vec3 f0, float alphaRoughness, float VdotH, float NdotL, float NdotV, float NdotH) {
-    vec3 F = F_Schlick(f0, VdotH);
-    float Vis = V_GGX(NdotL, NdotV, alphaRoughness);
-    float D = D_GGX(NdotH, alphaRoughness);
+// //  https://github.com/KhronosGroup/glTF/tree/master/specification/2.0#acknowledgments AppendixB
+// f16vec3 BRDF_specularGGX(f16vec3 f0, float16_talphaRoughness, float16_tVdotH, float16_tNdotL, float16_tNdotV, float16_tNdotH) {
+//     f16vec3 F = F_Schlick(f0, VdotH);
+//     float16_tVis = V_GGX(NdotL, NdotV, alphaRoughness);
+//     float16_tD = D_GGX(NdotH, alphaRoughness);
 
-    return F * Vis * D;
+//     return F * Vis * D;
+// }
+
+f16vec3 BRDF_specular(const f16vec3 f0, const float16_t roughness, f16vec3 h, const f16vec3 n, float16_t NdotV, float16_t NdotL, float16_t NdotH, float16_t LdotH) {
+    float16_t D = D_GGX(roughness, n, NdotH, h);
+    float16_t V = V_SmithGGXCorrelated_Fast(roughness, NdotV, NdotL);
+    f16vec3   F = F_Schlick(f0, LdotH);
+
+    return (D * V) * F;
 }
--- hotham/src/shaders/lights.glsl
+++ hotham/src/shaders/lights.glsl
@@ -1,38 +1,37 @@
-// Light related functionality, mostly borrowed from:
-// https://github.com/KhronosGroup/glTF-Sample-Viewer/blob/master/source/Renderer/shaders/punctual.glsl
+float16_t getSquareFalloffAttenuation(float16_t distanceSquare, float16_t falloff) {
+    float16_t factor = distanceSquare * falloff;
+    float16_t smoothFactor = saturate(F16(1.0) - factor * factor);
+    // // We would normally divide by the square distance here
+    // // but we do it at the call site
+    return smoothFactor * smoothFactor;
+}
 
-// https://github.com/KhronosGroup/glTF/blob/master/extensions/2.0/Khronos/KHR_lights_punctual/README.md#range-property
-float getRangeAttenuation(float range, float distance) {
-    if (range <= 0.0) {
-        // negative range means unlimited
-        return 1.0 / pow(distance, 2.0);
-    }
-    return max(min(1.0 - pow(distance / range, 4.0), 1.0), 0.0) / pow(distance, 2.0);
+float16_t getRangeAttenuation(const vec3 pointToLight, float16_t falloff) {
+    float16_t distanceSquare = F16(dot(pointToLight, pointToLight));
+    float16_t attenuation = getSquareFalloffAttenuation(distanceSquare, falloff);
+    // Assume a punctual light occupies a volume of 1cm to avoid a division by 0
+    return attenuation / max(distanceSquare, F16(1e-4));
 }
 
 // https://github.com/KhronosGroup/glTF/blob/master/extensions/2.0/Khronos/KHR_lights_punctual/README.md#inner-and-outer-cone-angles
-float getSpotAttenuation(vec3 pointToLight, vec3 spotDirection, float outerConeCos, float innerConeCos) {
-    float actualCos = dot(normalize(spotDirection), normalize(-pointToLight));
-    if (actualCos > outerConeCos) {
-        if (actualCos < innerConeCos) {
-            return smoothstep(outerConeCos, innerConeCos, actualCos);
-        }
-        return 1.0;
-    }
-    return 0.0;
+float16_t getSpotAttenuation(vec3 l, vec3 spotDirection, float16_t spotScale, float16_t spotOffset) {
+    float16_t cd = F16(dot(spotDirection, l));
+    float16_t attenuation = saturate(cd * spotScale + spotOffset);
+    return attenuation * attenuation;
+
 }
 
-vec3 getLightIntensity(Light light, vec3 pointToLight) {
-    float rangeAttenuation = 1.0;
-    float spotAttenuation = 1.0;
+float16_t getLightAttenuation(Light light, vec3 pointToLight, vec3 l) {
+    float16_t rangeAttenuation = F16(1.0);
+    float16_t spotAttenuation = F16(1.0);
 
     if (light.type != LightType_Directional) {
-        rangeAttenuation = getRangeAttenuation(light.range, length(pointToLight));
+        rangeAttenuation = getRangeAttenuation(pointToLight, F16(light.falloff));
     }
 
     if (light.type == LightType_Spot) {
-        spotAttenuation = getSpotAttenuation(pointToLight, light.direction, light.outerConeCos, light.innerConeCos);
+        spotAttenuation = getSpotAttenuation(l, -light.direction, F16(light.lightAngleScale), F16(light.lightAngleOffset));
     }
 
-    return rangeAttenuation * spotAttenuation * light.intensity * light.color;
+    return rangeAttenuation * spotAttenuation;
 }
