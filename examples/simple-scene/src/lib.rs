7bee6baedc6ed0ed95dfee8ede7faf683b5f56e1
Rasmus Brönnegård <1162652+rasmusgo@users.noreply.github.com>
2022-12-19 23:40:47 +0100

    Add debug_system to simple scene example

--- examples/simple-scene/src/lib.rs
+++ examples/simple-scene/src/lib.rs
@@ -3,9 +3,9 @@ use hotham::{
     components::{hand::Handedness, physics::SharedShape, Collider, LocalTransform, RigidBody},
     hecs::World,
     systems::{
-        animation_system, grabbing_system, hands::add_hand, hands_system, physics_system,
-        rendering::rendering_system, skinning::skinning_system, update_global_transform_system,
-        update_global_transform_with_parent_system,
+        animation_system, debug::debug_system, grabbing_system, hands::add_hand, hands_system,
+        physics_system, rendering::rendering_system, skinning::skinning_system,
+        update_global_transform_system, update_global_transform_with_parent_system,
     },
     xr, Engine, HothamResult, TickData,
 };
@@ -45,6 +45,7 @@ fn tick(tick_data: TickData, engine: &mut Engine, _state: &mut State) {
         update_global_transform_system(engine);
         update_global_transform_with_parent_system(engine);
         skinning_system(engine);
+        debug_system(engine);
     }
 
     rendering_system(engine, tick_data.swapchain_image_index);
