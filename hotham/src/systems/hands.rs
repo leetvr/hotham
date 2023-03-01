08b37e2fb3b225481f23f97186ace10bbddbb287
Kane Rogers <kanerogers@users.noreply.github.com>
2023-03-01 21:25:10 +1100

    Improvements to grabbing system. Closes #432 (#433)

--- hotham/src/systems/hands.rs
+++ hotham/src/systems/hands.rs
@@ -2,7 +2,7 @@ use crate::{
     asset_importer::add_model_to_world,
     components::{
         global_transform::GlobalTransform, hand::Handedness, local_transform::LocalTransform,
-        stage, AnimationController, Collider, Hand,
+        stage, AnimationController, Collider, Grabbed, Hand,
     },
     contexts::{physics_context::HAND_COLLISION_GROUP, InputContext},
     Engine,
@@ -52,11 +52,18 @@ pub fn hands_system_inner(world: &mut World, input_context: &InputContext) {
 
         // If we've grabbed something, update its transform, being careful to preserve its scale.
         if let Some(grabbed_entity) = hand.grabbed_entity {
-            let mut local_transform = world.get::<&mut LocalTransform>(grabbed_entity).unwrap();
-            local_transform.update_rotation_translation_from_affine(&global_from_local);
+            // We first need to check if some other system has decided that this item should no longer be grabbed.
+            if !world.entity(grabbed_entity).unwrap().has::<Grabbed>() {
+                hand.grabbed_entity = None;
+            } else {
+                // OK. We are sure that this entity exists, and is being grabbed.
+                let mut local_transform = world.get::<&mut LocalTransform>(grabbed_entity).unwrap();
+                local_transform.update_rotation_translation_from_affine(&global_from_local);
 
-            let mut global_transform = world.get::<&mut GlobalTransform>(grabbed_entity).unwrap();
-            *global_transform = (*local_transform).into();
+                let mut global_transform =
+                    world.get::<&mut GlobalTransform>(grabbed_entity).unwrap();
+                *global_transform = (*local_transform).into();
+            }
         }
 
         // Apply grip value to hand
@@ -133,6 +140,7 @@ mod tests {
 
         let expected_scale = Vec3::X * 1000.;
         let grabbed_entity = world.spawn((
+            Grabbed,
             RigidBody::default(),
             LocalTransform {
                 scale: expected_scale,
@@ -149,6 +157,25 @@ mod tests {
 
         // Make sure that scale gets preserved
         assert_relative_eq!(local_transform.scale, expected_scale);
+    }
+
+    #[test]
+    pub fn test_ungrabbed_object_do_not_move() {
+        let (mut world, input_context) = setup();
+
+        let grabbed_entity = world.spawn((
+            RigidBody::default(),
+            LocalTransform::default(),
+            GlobalTransform::default(),
+        ));
+        add_hand_to_world(&mut world, Some(grabbed_entity));
+
+        tick(&mut world, &input_context);
+
+        let local_transform = world.get::<&mut LocalTransform>(grabbed_entity).unwrap();
+        assert_relative_eq!(local_transform.translation, Default::default());
+        assert_relative_eq!(local_transform.scale, Vec3::ONE);
+        assert_relative_eq!(local_transform.rotation, Default::default());
     }
 
     // HELPER FUNCTIONS
